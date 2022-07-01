f2d14bb7738a51b5169bff89d060b359ef35cb35
artreysan <70738181+beartree-ars@users.noreply.github.com>
2022-07-01 01:01:11 -0500

    integracion de extension de validacion en express

--- src/routes/movies.js
+++ src/routes/movies.js
@@ -9,15 +9,21 @@ router.get('/',(req,res)=>{
     res.json(movies);
 });
 
-router.post('/',(req,res)=>{ //Resive un objeto del cliente
-    const  {tittle, director, year, rating } = req.body;
-    if(tittle && director && year && rating){
-        res.json('saved');
-    }
-    else{
-        res.send('Wrong Request');
-    }
+router.post('/',(req, res) => { //Recibe un objeto del cliente
+    console.log(req.body);
     res.send('Received');
+    /*
+    const id = movies.length + 1;
+    const { title, director, year, rating } = req.body;
+    const newMovie = { ...req.body, id };
+    if (id && title && director && year && rating) {
+        movies.push(newMovie);
+        res.json(movies);
+    } else {
+
+        res.status(500).json({error: 'There was an error.'});
+    }
+    */
 });
 
 module.exports =router;
